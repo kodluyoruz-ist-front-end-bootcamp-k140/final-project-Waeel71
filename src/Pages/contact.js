6d2c1dd4eb47caae3fdd9270b393caf6f618f6a1
wael <wael.derwish71@gmail.com>
2022-08-07 16:39:09 +0300

    bootcamp final project

--- src/Pages/contact.js
+++ src/Pages/contact.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 export function ContactPage (){
+    const savedArray = JSON.parse(localStorage.getItem("key"))
+    if(savedArray == null){return}
     return(
         <>
-        <h1>This is Contact page</h1>
+        {
+            <div className="card">
+                <img src={savedArray[0]} className="card-img-top" id="img" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title" id="title">{savedArray[1]}</h5>
+                    <p className="card-text" id="messege">{savedArray[2]}</p>
+                    {/* <button href="#" className="btn btn-primary">Add the lesson</button> */}
+                </div>
+            </div>
+        }
         </>
     )
 }
--- src/Pages/course.js
+++ src/Pages/course.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 export function CoursesPage() {
     const [data, setData] = useState([])
+    let image = ""
+    let title = ""
+    let messege = ""
     useEffect(() => {
-        fetch("https://api.jsonbin.io/v3/b/62ea6c6e70a3846ee752bb43?meta=false")
+        fetch("https://api.npoint.io/e4ebb20fefc1e75cab6d")
             .then((X) => { return X.json() })
             .then((response) => {
                 setData(response)
@@ -12,13 +15,19 @@ export function CoursesPage() {
         <>
             {data.sort((a, b) => a - b).map((item, index) => {
                 return (
-                    <div className="card" key={index}>
-                        <img src={item.imageUrl} className="card-img-top" alt="..." />
+                    <div className="card" key={index} onMouseDown={(e)=>{
+                        image = e.currentTarget.querySelector("#img").src
+                        title = e.currentTarget.querySelector("#title").innerHTML
+                        messege = e.currentTarget.querySelector("#messege").innerHTML
+                    }}>
+                        <img src={item.imageUrl} className="card-img-top" id="img" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">{item.name}</h5>
-                            <p className="card-text">{item.messege}</p>
-                            <button href="#" className="btn btn-primary" onClick={(e) => {
-                                console.log(e)
+                            <h5 className="card-title" id="title">{item.name}</h5>
+                            <p className="card-text" id="messege">{item.messege}</p>
+                            <button href="#" className="btn btn-primary" onClick={() => {
+                                const array = [image,title,messege]
+                                const savedArray = JSON.stringify(array)
+                                localStorage.setItem("key", savedArray)
                             }}>Add the lesson</button>
                         </div>
                     </div>
